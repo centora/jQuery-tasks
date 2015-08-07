d5c4d3d94b589411f26dea00c8d623f13e13d917
Centora <oksana.blonskaya@gmail.com>
2015-08-07 07:38:20 +0300

    Fixed js [0..01]

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 (function(){
 	'use strict';
 
-  $('ul li a').click(function(){
-    $('ul li a').css('background', "none");
-    $(this).css("background", "red");
-    return false;
-  });
+	$('ul li a').click(function(){
+		//$('ul li a').css('background', "none");
+		$(this).css("background", "red").parent().siblings().find('a').css('background', "none");;
+		//$(this).css("background", "red").parent().closest("li").find('a').css('background', "none");;
+		return false;
+	});
 
 })();
